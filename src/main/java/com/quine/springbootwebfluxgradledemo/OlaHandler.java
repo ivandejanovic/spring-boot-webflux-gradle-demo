1fba4fade20ce96e66814e8848990aca21fdc061
Ivan Dejanovic <idejanovic@gmail.com>
2020-03-10 21:23:41 +0100

    Initial project setup

---
+++ src/main/java/com/quine/springbootwebfluxgradledemo/OlaHandler.java
@@ -0,0 +1,17 @@
+package com.quine.springbootwebfluxgradledemo;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class OlaHandler {
+
+    public Mono<ServerResponse> hello(ServerRequest request) {
+        return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(BodyInserters.fromValue("Ola, Spring!"));
+    }
+}
